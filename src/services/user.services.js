2cc8df8129f96a66771bcee19e732d652988b1d5
David Felipe Santos Oliveira <davidfelipe_306@hotmail.com>
2020-06-09 22:48:50 -0400

    correcting sevice user

--- src/services/user.services.js
+++ src/services/user.services.js
@@ -1,9 +1,12 @@
-var User = require('../models/user')
+const User = require('../models/user')
 
 exports.getUsers = async function(query, page, limit) {
 
     try {
-        var users = await User.findAll(query)
+        var users = await User.findAll()
+        User.findAll().then((profile) => {
+            console.log(JSON.stringify(profile))
+        })
         return users;
     } catch (e) {
         console.log(e);
