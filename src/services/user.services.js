3af4e0e4248af5797cb893296483e957bf4e1463
David Felipe Santos Oliveira <davidfelipe_306@hotmail.com>
2020-06-09 22:55:55 -0400

    Revert "correcting sevice user"
    This reverts commit 2cc8df8129f96a66771bcee19e732d652988b1d5.

--- src/services/user.services.js
+++ src/services/user.services.js
@@ -1,12 +1,9 @@
-const User = require('../models/user')
+var User = require('../models/user')
 
 exports.getUsers = async function(query, page, limit) {
 
     try {
-        var users = await User.findAll()
-        User.findAll().then((profile) => {
-            console.log(JSON.stringify(profile))
-        })
+        var users = await User.findAll(query)
         return users;
     } catch (e) {
         console.log(e);
